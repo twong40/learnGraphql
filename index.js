b16497302e03293c948003581bc6a1e0447a059c
thomaswong1000@gmail.com <twong40@ucsc.edu>
2020-05-07 16:23:28 -0700

    added more mutations to graphql example

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { ApolloServer, gql } = require("apollo-server");
+const { ApolloServer, UserInputError, gql } = require("apollo-server");
 const { v1: uuid } = require("uuid");
 let persons = [
   {
@@ -35,9 +35,14 @@ const typeDefs = gql`
     id: ID!
   }
 
+  enum YesNo {
+    YES
+    NO
+  }
+
   type Query {
     personCount: Int!
-    allPersons: [Person!]!
+    allPersons(phone: YesNo): [Person!]!
     findPerson(name: String!): Person
   }
   type Mutation {
@@ -47,13 +52,19 @@ const typeDefs = gql`
       street: String!
       city: String!
     ): Person
+    editNumber(name: String!, phone: String!): Person
   }
 `;
 
 const resolvers = {
   Query: {
     personCount: () => persons.length,
-    allPersons: () => persons,
+    allPersons: (root, args) => {
+      if (!args.phone) return persons;
+      const byPhone = (person) =>
+        args.phone === "YES" ? person.phone : !person.phone;
+      return persons.filter(byPhone);
+    },
     findPerson: (root, args) => persons.find((p) => p.name === args.name),
   },
   Person: {
@@ -63,9 +74,23 @@ const resolvers = {
   },
   Mutation: {
     addPerson: (root, args) => {
+      if (persons.find((p) => p.name === args.name)) {
+        throw new UserInputError("Name must be unique", {
+          invalidArgs: args.name,
+        });
+      }
       const person = { ...args, id: uuid() };
       persons = persons.concat(person);
       return person;
+    },
+
+    editNumber: (root, args) => {
+      const persons = persons.find((p) => p.name === args.name);
+      if (!person) return null;
+
+      const updatedPerson = { ...person, phone: args.phone };
+      person = persons.map((p) => (p.name === args.name ? updatedPerson : p));
+      return updatedPerson;
     },
   },
 };
